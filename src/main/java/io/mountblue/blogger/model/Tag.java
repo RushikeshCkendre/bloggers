60ee0403a63a6588a3544adefad1d475eee4fd91
rushi-codes <rushikesh.kendre.34.2@mountblue.tech>
2025-10-06 15:04:45 +0530

    tag model created

---
+++ src/main/java/io/mountblue/blogger/model/Tag.java
@@ -0,0 +1,28 @@
+package io.mountblue.blogger.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tags")
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
