b7767f172d845371f6f0443107f361a7145da030
rushi-codes <rushikesh.kendre.34.2@mountblue.tech>
2025-10-06 15:21:22 +0530

    comment model created

---
+++ src/main/java/io/mountblue/blogger/model/Comment.java
@@ -0,0 +1,30 @@
+package io.mountblue.blogger.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String comment;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
